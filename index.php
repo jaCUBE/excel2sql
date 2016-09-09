5e34f23954a740b8e67e028a7c5816e280cb37f5
jaCUBE <jacube.cz@gmail.com>
2016-09-09 16:57:32 +0200

    Google Analytics code has been added.

--- index.php
+++ index.php
@@ -24,8 +24,19 @@ require 'bootstrap.php';
     
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     
-    <script src="<?php echo URL; ?>/js/excel2sql.min.js"></script>
-    <link href="<?php echo URL; ?>/css/excel2sql.css" rel="stylesheet">
+    <script src="<?= URL; ?>/js/excel2sql.min.js"></script>
+    <link href="<?= URL; ?>/css/excel2sql.css" rel="stylesheet">
+    
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-33858105-10', 'auto');
+      ga('send', 'pageview');
+
+    </script>
   </head>
   
   
@@ -34,7 +45,7 @@ require 'bootstrap.php';
       <div id="header">
         <div id="logotype">
           <a href="index.php">
-            <img src="<?php echo URL; ?>/images/logotype.png" alt="Excel2SQL" />
+            <img src="<?= URL; ?>/images/logotype.png" alt="Excel2SQL" />
           </a>
         </div>
       </div>
--- views/form.view.php
+++ views/form.view.php
@@ -20,7 +20,7 @@
     
     <form action="#" method="post" id="form-main">  
       <label for="excel-input">Copy 'n' paste your Excel table here:</label>
-      <textarea name="excel_input" id="excel-input" class="form-control form-save"><?php echo @$_POST['excel_input']; // Prints input after form submitting ?></textarea>
+      <textarea name="excel_input" id="excel-input" class="form-control form-save"><?= @$_POST['excel_input']; // Prints input after form submitting ?></textarea>
 
       <div class="center">
         <button type="submit" class="btn btn-primary btn-lg">
@@ -40,13 +40,13 @@
     </h2>
     
     <label for="sql-output">Voilà, here is your SQL insert queries:</label>
-    <textarea name="sql_output" id="sql-output" class="form-control form-save" readonly><?php if(is_a($this->data, 'models\Table')) echo $this->data->toSql(); // Generated SQL queries ?></textarea>
+    <textarea name="sql_output" id="sql-output" class="form-control form-save" readonly><?= is_a($this->data, 'models\Table') ? $this->data->toSql() : '' // Generated SQL queries ?></textarea>
 
     
     <?php if(!empty($this->data->rows)){ // If there are any processed data rows... ?>
       <div class="right">
         <small>
-          Generate <?php echo count($this->data->rows); // Count of data rows ?> queries took <?php echo round(elapsedTime(), 3); // Elapsed time from script start ?> seconds.
+          Generate <?= count($this->data->rows); // Count of data rows ?> queries took <?= round(elapsedTime(), 3); // Elapsed time from script start ?> seconds.
         </small>
       </div>
     <?php } ?>
